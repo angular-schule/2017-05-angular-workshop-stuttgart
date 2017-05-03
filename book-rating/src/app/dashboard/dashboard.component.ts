132eef3d5af7c813151cc34ed49c29c01f1851d7
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-05-03 13:39:44 +0200

    don't forget to unsubscribe!

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,18 +1,21 @@
 import { BookComponent } from './../book/book.component';
 import { ViewChild } from '@angular/core';
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { Book } from '../shared/book';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'br-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit, AfterViewInit {
+export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy {
   books: Book[];
 
   @ViewChild(BookComponent)
   firstBookComponent: BookComponent;
+
+  subscription: Subscription;
 
   constructor() { }
 
@@ -26,9 +29,13 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.firstBookComponent.rated.subscribe((book: Book) => {
+    this.subscription = this.firstBookComponent.rated.subscribe((book: Book) => {
       console.log(book);
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   reorderBooks() {
