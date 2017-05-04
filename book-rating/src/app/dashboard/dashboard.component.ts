43290d01c56570ca885b402f1cf815aee6d23008
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-05-04 15:55:10 +0200

    how to use async pipe

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs/Observable';
+import { BookStoreService } from './../shared/book-store.service';
 import { BookComponent } from './../book/book.component';
 import { ViewChild } from '@angular/core';
 import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
@@ -11,12 +13,15 @@ import { Subscription } from 'rxjs/Subscription';
 })
 export class DashboardComponent implements OnInit {
   books: Book[] = [];
+  books$: Observable<Book[]>;
 
   subscription: Subscription;
 
   constructor(private bs: BookStoreService) { }
 
   ngOnInit() {
+    this.bs.getAll()
+      .subscribe(books => this.books = books);
 
     this.books$ = this.bs.getAll()
 
