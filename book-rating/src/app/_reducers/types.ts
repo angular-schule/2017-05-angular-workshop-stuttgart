d8b75dcfc151ca8e25f50889f1cb63762f1283ff
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-05-18 21:39:16 +0200

    TODO: implement books reducer and actions!

--- book-rating/src/app/_reducers/types.ts
+++ book-rating/src/app/_reducers/types.ts
@@ -1,5 +1,6 @@
 import * as si from 'seamless-immutable';
 import { Book } from '../shared/book';
+import { Action } from 'redux';
 
 export interface IAppState {
 
@@ -16,3 +17,7 @@ export type BookState = si.Immutable<{
   books: Book[];
   isLoading: boolean;
 }>;
+
+export interface ActionPayload<T> extends Action {
+  payload?: T;
+}
